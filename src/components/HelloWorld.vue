3477c4622efc3974deb7f7c232c42250e71ff399
dong <1223519172@qq.com>
2022-05-25 22:22:12 +0800

    refactor: 闹钟功能和主体代码拆分。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,10 @@
 <template>
-<!--  <h1>{{ msg }}</h1>-->
-<!--  <el-date-picker-->
-<!--      v-model="ringTime"-->
-<!--      type="datetime"-->
-<!--      placeholder="选择响铃时间"-->
-<!--      value-format="x"-->
-<!--      :disabled="started"-->
-<!--  />-->
-<!--  <el-button type="success" :disabled="started" @click="start">确定</el-button>-->
-  <aggregate-search></aggregate-search>
+  {{ msg }}
   <upload-file></upload-file>
 </template>
 
 <script lang="ts">
-import {Ref, defineComponent, ref} from 'vue'
-import {textToSpeech} from './../utils/index'
-import request from '../request'
-import aggregateSearch from './aggregateSearch/index.vue'
+import { defineComponent } from 'vue'
 import uploadFile from './uploadFile/index.vue'
 
 export default defineComponent({
@@ -28,50 +16,11 @@ export default defineComponent({
   },
 
   components: {
-    aggregateSearch,
     uploadFile
   },
 
   setup() {
-
-
-
-    // 响铃时间
-    const ringTime = ref(0)
-    let timer: Ref<NodeJS.Timer | undefined> = ref()
-    let started = ref(false)
-
-    const handle = () => {
-      console.log('handle 执行了')
-      // 当前时间大于/等于响铃时间则响铃
-      if (Date.now() >= ringTime.value) {
-        console.log('响铃啦~')
-        // const audio = document.createElement('audio')
-        // audio.autoplay = true
-        // audio.src = 'https://other-web-nf01-sycdn.kuwo.cn/934895110ac2c890b9a1bfceb8971c66/627f6511/resource/n2/55/36/3027403034.mp3'
-        // document.body.append(audio)
-        textToSpeech('xxxx')
-
-        clearInterval(timer.value)
-        timer.value = undefined
-        started.value = false
-      }
-    }
-
-    const start = () => {
-      console.log(ringTime.value)
-      if (!timer.value) {
-        started.value = true
-        timer.value = setInterval(handle, 1000)
-      }
-    }
-
-    return {
-      started,
-      timer,
-      start,
-      ringTime
-    }
+    return {}
   }
 })
 
