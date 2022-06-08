40aaef3dd891ea27f37d05f5f0701577e2be595f
dong <1223519172@qq.com>
2022-06-08 22:32:23 +0800

    feat: 引入 JSX

---
+++ src/components/jsx/Heading.jsx
@@ -0,0 +1,14 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    level: {
+      type: Number,
+      required: true
+    }
+  },
+  setup(props, { slots }) {
+    const tag = `h${ props.level }`
+    return () => <tag>{ slots.default }</tag>
+  }
+})
