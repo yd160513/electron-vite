8c2ef79b75ced85ef9a0ae3f6d840a45402089ee
dong <1223519172@qq.com>
2022-06-08 22:35:41 +0800

    feat: 学习定义 props 和 emit

--- src/components/count/index.vue
+++ src/components/count/index.vue
@@ -3,9 +3,14 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, defineProps, defineEmits } from 'vue'
 // import { useStore } from '../../store/gvuex'
 import { useStore } from 'vuex'
+
+const props = defineProps<{ title: string, value: number }>()
+const emit = defineEmits<{
+  (e: 'update', value: number): void
+}>()
 
 const store = useStore()
 const count = computed(() => store.state.count)
